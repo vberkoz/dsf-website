e9bcde0e2c041beb251776ba4b92a9bb60a42634
vberkoz <vberkoz@gmail.com>
2023-02-07 15:48:05 +0200

    Add next steps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { Benefits } from "@/components/Home/Benefits/Benefits";
 import { Carousel } from "@/components/Home/Carousel/Carousel";
 import { Features } from "@/components/Home/Features/Features";
 import { Hero } from "@/components/Home/Hero/Hero";
+import { NextSteps } from "@/components/Home/NextSteps/NextSteps";
 import { Layout } from "@/components/Layout";
 
 export default function Home() {
@@ -9,7 +11,9 @@ export default function Home() {
       <Carousel />
       <Features />
       <Hero index={0} />
+      <Benefits />
       <Hero index={1} />
+      <NextSteps />
     </Layout>
   );
 }
