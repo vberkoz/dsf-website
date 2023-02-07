a6f80a3736aea3534505ea7d400d7af07119a61f
vberkoz <vberkoz@gmail.com>
2023-02-08 00:18:15 +0200

    Add HeroTabs component

---
+++ components/HeroTabs.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import sunset from "@/public/about/sunset.webp";
+
+export const HeroTabs = () => {
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${sunset.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="bg-gradient-to-r from-gray-900/100 to-gray-900/50 text-left text-white lg:to-gray-900/0">
+          <div className="pl-14">
+            <div className="p-3">
+              <h1 className="mb-4 text-4xl font-light sm:text-5xl">About Us</h1>
+              <div className="grid pb-40 md:grid-cols-2 lg:grid-cols-3">
+                <p className="text-white/80"></p>
+              </div>
+            </div>
+          </div>
+
+          <div className="border-b-2 border-gray-800">
+            <div className="ml-14 flex text-white">
+              <Link
+                href="#"
+                className="border-t-2 border-yellow-400 bg-gray-800 p-3 hover:bg-gray-700"
+              >
+                About us
+              </Link>
+              <Link href="#" className="p-3 hover:bg-gray-700">
+                Our team's story
+              </Link>
+              <Link href="#" className="p-3 hover:bg-gray-700">
+                On-demand economy
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
