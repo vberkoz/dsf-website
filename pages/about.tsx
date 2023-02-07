808d388f987fc80451a1974ece4eb0159047286c
vberkoz <vberkoz@gmail.com>
2023-02-08 00:18:47 +0200

    Update About Us page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
+import { HeroTabs } from "@/components/HeroTabs";
 import { NextSteps } from "@/components/Home/NextSteps/NextSteps";
 import { Layout } from "@/components/Layout";
+import Link from "next/link";
 
 export default function About() {
   return (
     <Layout>
-      <p>About us</p>
+      <HeroTabs />
       <NextSteps />
     </Layout>
-  )
+  );
 }
