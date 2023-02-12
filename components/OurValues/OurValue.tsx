808c73ca4df83c25022d82b478cbf24d1e5b0323
vberkoz <vberkoz@gmail.com>
2023-02-12 11:36:07 +0200

    Update OurValues styles

--- components/OurValues/OurValue.tsx
+++ components/OurValues/OurValue.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const OurValue = ({ ourValue }: Props) => (
   <div className="flex flex-col text-gray-400">
-    <div className="h-20 text-3xl font-light text-white">{ourValue.title}</div>
+    <div className="h-20 text-3xl text-white">{ourValue.title}</div>
 
     <div className="grid grid-cols-1 gap-4">
       {ourValue.content.map((item, key) => (
