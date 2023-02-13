fe270e8081b082d4bafe7e70c314272bbdb8c963
vberkoz <vberkoz@gmail.com>
2023-02-14 00:16:34 +0200

    Convert Menu component to digest dynamic data

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -3,41 +3,51 @@ import Link from "next/link";
 import logo from "../../public/logo.svg";
 import { useState } from "react";
 import { MobileMenuItem } from "./MobileMenuItem";
-import { MenuItemData } from "./MenuItemData";
+
+import { Pages } from "@/data/data";
 
 export const Menu = () => {
   const [opened, setOpened] = useState(false);
-  const menuItems = MenuItemData;
 
   const toggleMenu = () => {
     setOpened(!opened);
   };
 
+  const pagesets = Pages.filter((page) => page.isPrimary);
+  const menuItems = pagesets.map((item) => {
+    const subpages = Pages.filter((page) => (page.pageset === item.pageset && !page.isPrimary));
+    const subitems = subpages.map((page) => ({
+      title: page.title,
+      href: page.slug,
+    }));
+    
+    if (subitems.length) {
+      return {
+        title: item.title,
+        href: item.slug,
+        subitems: subitems,
+      }
+    } else {
+      return {
+        title: item.title,
+        href: item.slug,
+      }
+    }
+  });
+
   return (
-    <div style={{zIndex: 2000}} className="sticky top-0 flex justify-between border-b border-gray-200 bg-white text-black">
-      <div
-        onClick={toggleMenu}
-        className={`${
-          opened ? "block" : "hidden"
-        } absolute top-0 left-0 z-0 h-[100vh] w-[100vw] bg-black/50`}
-      ></div>
+    <div style={{ zIndex: 2000 }} className="sticky top-0 flex justify-between border-b border-gray-200 bg-white text-black">
+      <div onClick={toggleMenu} className={`${ opened ? "block" : "hidden" } absolute top-0 left-0 z-0 h-[100vh] w-[100vw] bg-black/50`}></div>
 
       <div className="flex">
         <div className="z-10 float-left lg:hidden">
-          <div
-            onClick={toggleMenu}
-            className="z-10 cursor-pointer bg-white p-3"
-          >
+          <div onClick={toggleMenu} className="z-10 cursor-pointer bg-white p-3">
             <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20">
               <path d="M2 14.8H18V16H2zM2 11.2H18V12.399999999999999H2zM2 7.6H18V8.799999999999999H2zM2 4H18V5.2H2z"></path>
             </svg>
           </div>
 
-          <div
-            className={`${
-              opened ? "block" : "hidden"
-            } absolute h-auto w-full bg-white sm:w-80`}
-          >
+          <div className={`${ opened ? "block" : "hidden" } absolute h-auto w-full bg-white sm:w-80`}>
             <div className="relative h-full w-full overflow-auto overscroll-contain">
               <div className="relative flex h-[80vh] flex-col">
                 {menuItems.map((item, key) => (
@@ -46,9 +56,7 @@ export const Menu = () => {
                       <MobileMenuItem ddItem={item} />
                     ) : (
                       <div className="flex flex-col">
-                        <Link href={item.href} className="grow p-3">
-                          {item.title}
-                        </Link>
+                        <Link href={item.href} className="grow p-3">{item.title}</Link>
                       </div>
                     )}
                   </div>
@@ -72,37 +80,20 @@ export const Menu = () => {
               <div key={key} className="flex flex-col">
                 {item?.subitems ? (
                   <div className="group">
-                    <Link
-                      href={item.href}
-                      className="flex cursor-pointer p-3 hover:bg-gray-200"
-                    >
+                    <Link href={item.href} className="flex cursor-pointer p-3 hover:bg-gray-200">
                       {item.title}
-                      <svg
-                        className="m-1"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                      >
+                      <svg className="m-1" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
                         <path d="M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z"></path>
                       </svg>
                     </Link>
                     <div className="absolute z-10 hidden flex-col bg-white shadow group-hover:flex">
                       {item.subitems.map((subItem, subItemKey) => (
-                        <Link
-                          key={subItemKey}
-                          href={subItem.href}
-                          className="p-3 hover:bg-gray-200"
-                        >
-                          {subItem.title}
-                        </Link>
+                        <Link key={subItemKey} href={subItem.href} className="p-3 hover:bg-gray-200">{subItem.title}</Link>
                       ))}
                     </div>
                   </div>
                 ) : (
-                  <Link href={item.href} className="p-3 hover:bg-gray-200">
-                    {item.title}
-                  </Link>
+                  <Link href={item.href} className="p-3 hover:bg-gray-200">{item.title}</Link>
                 )}
               </div>
             ))}
@@ -111,9 +102,7 @@ export const Menu = () => {
       </div>
 
       <div className="flex">
-        <Link href="#" className="bg-yellow-300 p-3 hover:bg-yellow-400">
-          Submit GIG
-        </Link>
+        <Link href="#" className="bg-yellow-300 p-3 hover:bg-yellow-400">Submit GIG</Link>
       </div>
     </div>
   );
--- components/Menu/MobileMenuItem.tsx
+++ components/Menu/MobileMenuItem.tsx
@@ -1,19 +1,15 @@
 import Link from "next/link";
 import { useState } from "react";
 
-type MenuItem = {
-  title: string;
-  href: string;
-  subitems?: SubMenuItem[];
-};
-
-type SubMenuItem = {
-  title: string;
-  href: string;
-};
-
 type Props = {
-  ddItem: MenuItem;
+  ddItem: {
+    title: string;
+    href: string;
+    subitems?: {
+      title: string;
+      href: string;
+    }[];
+  };
 };
 
 export const MobileMenuItem = ({ ddItem }: Props) => {
@@ -26,35 +22,18 @@ export const MobileMenuItem = ({ ddItem }: Props) => {
   return (
     <div className="flex flex-col">
       <div className="flex">
-        <Link href={ddItem.href} className="grow px-4 py-3">
-          {ddItem.title}
-        </Link>
+        <Link href={ddItem.href} className="grow px-4 py-3">{ddItem.title}</Link>
 
-        <div
-          className={`${opened ? "bg-gray-100" : ""} cursor-pointer p-2`}
-          onClick={toggleMenu}
-        >
-          <svg
-            fill="currentColor"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            className="m-2"
-          >
+        <div className={`${opened ? "bg-gray-100" : ""} cursor-pointer p-2`} onClick={toggleMenu}>
+          <svg fill="currentColor" width="16" height="16" viewBox="0 0 16 16" className="m-2">
             <path d="M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z"></path>
           </svg>
         </div>
       </div>
 
-      <div
-        className={`${
-          opened ? "flex" : "hidden"
-        } w-full flex-col bg-white pl-6`}
-      >
+      <div className={`${ opened ? "flex" : "hidden" } w-full flex-col bg-white pl-6`}>
         {ddItem?.subitems?.map((subItem, key) => (
-          <Link key={key} href={subItem.href} className="bg-gray-100 px-4 py-3">
-            {subItem.title}
-          </Link>
+          <Link key={key} href={subItem.href} className="bg-gray-100 px-4 py-3">{subItem.title}</Link>
         ))}
       </div>
     </div>
