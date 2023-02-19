fe9ee6d7b88771399c2abd987048443e6bb05580
vberkoz <vberkoz@gmail.com>
2023-02-19 14:36:42 +0200

    Update XitStrategy page

--- data/pagesets/XitStrategy.ts
+++ data/pagesets/XitStrategy.ts
@@ -1,3 +1,4 @@
+import xitStrategy from "@/public/xitStrategy.webp";
 import { DefaultNextSteps } from "../DefaultNextSteps";
 
 export const XitStrategy = {
@@ -5,7 +6,12 @@ export const XitStrategy = {
   slug: "xit-strategy",
   pageset: "xit-strategy",
   isPrimary: true,
+  description: "ALL THINGS CAPITAL: EXIT Strategies, Intellectual Property, Looking to sell your company, Stuff your Sales Pipe to Raise Capital",
+  image: xitStrategy.src,
   sections: [
+    {
+      id: "HeroTabs",
+    },
     {
       id: "NextSteps",
       steps: DefaultNextSteps
