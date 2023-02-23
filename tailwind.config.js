8826f5c049cd4ef2201dd594ffc8ef8da30a4e30
vberkoz <vberkoz@gmail.com>
2023-02-23 21:06:25 +0200

    Remove hover effect on touch screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,34 @@
+
+
+const plugin = require('tailwindcss/plugin');
+
+const hoverPlugin = plugin(function({ addVariant, e, postcss }) {
+    addVariant('hover', ({ container, separator }) => {
+        const hoverRule = postcss.atRule({ name: 'media', params: '(hover: hover)' });
+        hoverRule.append(container.nodes);
+        container.append(hoverRule);
+        hoverRule.walkRules(rule => {
+            rule.selector = `.${e(`hover${separator}${rule.selector.slice(1)}`)}:hover`
+        });
+    });
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)'],
+        sans: ["var(--font-sans)"],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [hoverPlugin],
+};
