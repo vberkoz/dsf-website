347538bbb74c86b3e161d052b00c53f87ef88437
vberkoz <vberkoz@gmail.com>
2023-03-01 14:03:21 +0200

    Update Sales360Team page

--- data/pagesets/JoinOurTeam/Sales360Team.ts
+++ data/pagesets/JoinOurTeam/Sales360Team.ts
@@ -1,20 +1,194 @@
 import { DefaultNextSteps } from "@/data/DefaultNextSteps";
 import writing from "@/public/join_our_team/writing.webp";
+import team from "@/public/join_our_team/team.webp";
+
+import home from "@/public/join_our_team/home.svg";
+import rocket from "@/public/join_our_team/rocket.svg";
+import archive from "@/public/join_our_team/archive.svg";
+import university from "@/public/join_our_team/university.svg";
+import support from "@/public/join_our_team/support.svg";
+import presentation from "@/public/join_our_team/presentation.svg";
+import tools from "@/public/join_our_team/tools.svg";
+
+import bulb from "@/public/home/bulb.svg";
 
 export const Sales360Team = {
   title: "Sales 360 team",
   slug: "sales-360-team",
   pageset: "join-our-team",
   isPrimary: false,
-  description: "VO Group allows an environment for the professional to have the Gig Life",
+  description:
+    "VO Group allows an environment for the professional to have the Gig Life",
   image: writing.src,
   sections: [
     {
       id: "HeroTabs",
     },
     {
+      id: "ContentC2DE",
+      caption: [],
+      title: "Career - Self Employment",
+      svg: home.src,
+      content: [
+        "The VO Sales 360 Team is comprised of Business Development Managers (BDs) - professional salespeople, seasoned in making high-level calls into an organization.",
+        "VO Group's Self Employment Career Opportunity allows for self-employed professionals to work at home.",
+        "Many of VO Group's Sales 360 Team contractors are women and moms who have chosen a work-at-home lifestyle for a variety of reasons.",
+        "VO Group creates a collaborative virtual environment using readily available cloud and collaboration tools. In today's On-Demand Economy, VO Group allows an environment for the professional to have the Gig Life.",
+        "VO Group offers a unique model, built around collaborative business tools that allow the self-employed Gigger or Tasker to feel part of a company, yet work individually.",
+        "VO Group's contractors have chosen a work-at-home lifestyle for a variety of reasons and are able to effectively balance their work-life with their family life.",
+      ],
+      link: "",
+      href: "#",
+      image: null,
+    },
+    {
+      id: "Hero",
+      image: team.src,
+      title: "Sales 360 Team Benefits",
+      content: [
+        "As a Business Development Manager (BD) at VO Group, we give you all the tools to be in business for yourself as an independent contractor.",
+        "With our national reputation, you can have your own lead generation business. We will give you the tools and resources to be successful.",
+        "If you can prospect, we want to talk to you.",
+      ],
+      link: "Sales team testimonials",
+      href: "/sales-team-testimonials",
+    },
+    {
+      id: "Content121",
+      caption: [],
+      title: "Background Knowledge",
+      content: [
+        "We are looking for those who have an understanding of M2M, mHealth, Supply Chain, RFID/Logistics, Security Products, Retail/POS, Enterprise Mobility, Enterprise Level Software Solutions, Managed Services, Cloud Computing and other high-value solutions.",
+        "We are also looking for those that understand verticals, such as Retail, Healthcare, Financial, Casinos, Hospitality, Manufacturing, etc.",
+        "Your own vertical market understanding is important to us.",
+      ],
+      link: "",
+      href: "#",
+      image: null,
+    },
+    {
+      id: "Content1111",
+      data: {
+        caption: "",
+        title1: "Required Skills",
+        title2: "Business Acumen",
+        col1: [
+          [
+            "Sales skills and sales strategies are a must",
+            "A technology or business solution-centric sales background is required",
+            "This is a contract position, with no guarantee of hours",
+            "This is a performance-based environment",
+            "We do offer hourly pay with a bonus",
+          ],
+        ],
+        col2: [
+          "Strong business acumen and a strong sales background are more important than product knowledge.",
+          "Our style is to focus on the solution, the business solution more than the product offering itself.",
+        ],
+      },
+    },
+    {
+      id: "ContentC2DE",
+      caption: [],
+      title: "VO Group's Model for Success",
+      svg: rocket.src,
+      content: [
+        "We help you with all the tools necessary to be a self-employed sales development contractor.",
+        [
+          "We offer training on our processes",
+          "We offer product and solutions training",
+          "We will keep a steady flow of new accounts for you to vie for",
+          "We pay early and we pay weekly - we always use direct deposit",
+          "We become your collection arm",
+          "We give you the tools and resources to report your results",
+          "We offer call list support for you to be successful in doing what you do best - sales prospecting/sales development",
+          "We have a PO issuance and Project Tracking program that you will use to streamline your efforts to allow you to scale",
+          "We take care of all contracts with new clients",
+        ],
+      ],
+      link: "",
+      href: "#",
+      image: null,
+    },
+    {
+      id: "ContentC2DE",
+      caption: [],
+      title: "Additional Benefits",
+      svg: archive.src,
+      content: [
+        "We have a back-end team to manage a campaign, do reporting, and log time.",
+        [
+          "We have a routine to help with campaign renewals",
+          "We pay early and we pay weekly - we embrace direct deposit",
+          "We become your collection arm and your administrative back-end",
+          "We will pay any subs that you have working on your project, and do their 1099s",
+          "We have a PO issuance and Project Tracking program that you will use to streamline your efforts to allow you to scale",
+          "We take care of all contracts with new clients or new subs",
+        ],
+        "VO Group takes the peaks and valleys out of your traditional freelance gigs.",
+      ],
+      link: "",
+      href: "#",
+      image: null,
+    },
+    {
+      id: "ContentCDDE",
+      caption: [],
+      data: [
+        {
+          title: "Knowledge of Sales and Marketing",
+          svg: university.src,
+          content: [
+            "VO Group is not a self-serve model. You must request an interview and have knowledge of how to drive sales for solution providers. All of our customers sell High-Value Solutions.",
+            "Our job is to drive revenue for them. Knowledge of Sales and Marketing in the vendor space is a requirement. Request an Interview and get on the VO Map.",
+          ],
+          link: "",
+          href: "#",
+        },
+        {
+          title: "Administrative Support",
+          svg: support.src,
+          content: [
+            "VO Group offers administrative support for independent contractors.",
+            "You can still be self-employed, but allow VO Group to support your independent contractor efforts.",
+            "We win business and then assign accounts to you. All of the administrative support comes from VO Group, you can just do what you do best.",
+          ],
+          link: "",
+          href: "#",
+        },
+      ],
+    },
+    {
+      id: "ContentCDDE",
+      caption: [],
+      data: [
+        {
+          title: "What we Offer",
+          svg: presentation.src,
+          content: [
+            "We offer an environment of self-employment and flex hours.",
+            "This opportunity comes with the potential to earn $40K, $60K, or $80K, depending on duties performed and your success at those duties.",
+            "Six figures can be achieved if one tackles all aspects of our program: prospecting, managing, and new account acquisition.",
+          ],
+          link: "",
+          href: "#",
+        },
+        {
+          title: "Tools",
+          svg: tools.src,
+          content: [
+            "VO gives our Business Development Managers (BDs) the tools to grow their own business.",
+            "With our client relationships, we can smooth out your income by allowing you to vie for accounts as they come up.",
+            "We are doing your selling to keep your gig pipeline full.",
+          ],
+          link: "",
+          href: "#",
+        },
+      ],
+    },
+    {
       id: "NextSteps",
-      steps: DefaultNextSteps
+      steps: DefaultNextSteps,
     },
   ],
 };
