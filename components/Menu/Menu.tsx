9038cbded58ec0fdc4a7e87d45c42c31bf8658c8
vberkoz <vberkoz@gmail.com>
2023-03-03 12:34:40 +0200

    Update Menu dark mode

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -41,18 +41,18 @@ export const Menu = () => {
   const currentPageset = Pages.filter(page => page.slug === currentPage);
 
   return (
-    <div style={{ zIndex: 2001 }} className="sticky top-0 flex justify-between bg-gray-100 text-gray-900 border-b-2 border-gray-200 dark:border-gray-800">
+    <div style={{ zIndex: 2001 }} className="sticky top-0 flex justify-between bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 border-b-2 border-gray-200 dark:border-gray-800">
       <div onClick={toggleMenu} className={`${ opened ? "block" : "hidden" } absolute top-0 left-0 z-0 h-[100vh] w-[100vw] bg-gray-900/50`}></div>
 
       <div className="flex">
         <div className="z-10 float-left lg:hidden">
-          <div onClick={toggleMenu} className="z-10 cursor-pointer bg-gray-100 p-3">
+          <div onClick={toggleMenu} className="z-10 cursor-pointer bg-gray-100 dark:bg-gray-900 p-3">
             <svg fill="currentColor" width="20" height="20" viewBox="0 0 20 20">
               <path d="M2 14.8H18V16H2zM2 11.2H18V12.399999999999999H2zM2 7.6H18V8.799999999999999H2zM2 4H18V5.2H2z"></path>
             </svg>
           </div>
 
-          <div className={`${ opened ? "block" : "hidden" } absolute h-auto w-full bg-gray-100 sm:w-80`}>
+          <div className={`${ opened ? "block" : "hidden" } absolute h-auto w-full bg-gray-100 dark:bg-gray-900 sm:w-80`}>
             <div className="relative h-full w-full overflow-auto overscroll-contain">
               <div className="relative flex h-[80vh] flex-col">
                 {menuItems.map((item, key) => (
@@ -71,34 +71,34 @@ export const Menu = () => {
           </div>
         </div>
 
-        <div className="my-3 border-r border-gray-200"></div>
+        <div className="my-3 border-r border-gray-200 dark:border-gray-800"></div>
 
-        <Link className="py-2 px-3 hover:bg-gray-200" href="/">
+        <Link className="py-2 px-3 hover:bg-gray-200 dark:hover:bg-gray-800" href="/">
           <Image src={logo} alt="VO Group" width={30} height={30} />
         </Link>
 
         <div className="hidden lg:flex">
-          <div className="my-3 border-r border-gray-200"></div>
+          <div className="my-3 border-r border-gray-200 dark:border-gray-800"></div>
 
           <div className="float-left flex">
             {menuItems.map((item, key) => (
               <div key={key} className="flex flex-col">
                 {item?.subitems ? (
                   <div className="group">
-                    <Link href={`/${item.href}`} className={`${ item.href === currentPageset[0]?.pageset && "bg-gray-200 dark:bg-gray-700" } flex cursor-pointer p-3 hover:bg-gray-200`}>
+                    <Link href={`/${item.href}`} className={`${ item.href === currentPageset[0]?.pageset && "bg-gray-200 dark:bg-gray-700" } flex cursor-pointer p-3 hover:bg-gray-200 dark:hover:bg-gray-800`}>
                       {item.title}
                       <svg className="m-1" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
                         <path d="M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z"></path>
                       </svg>
                     </Link>
-                    <div className="absolute z-10 hidden flex-col bg-gray-100 shadow group-hover:flex">
+                    <div className="absolute z-10 hidden flex-col bg-gray-100 dark:bg-gray-900 shadow group-hover:flex">
                       {item.subitems.map((subItem, subItemKey) => (
-                        <Link key={subItemKey} href={`/${subItem.href}`} className={`${ subItem.href === currentPage && "bg-gray-200 dark:bg-gray-700" } flex cursor-pointer p-3 hover:bg-gray-200`}>{subItem.title}</Link>
+                        <Link key={subItemKey} href={`/${subItem.href}`} className={`${ subItem.href === currentPage && "bg-gray-200 dark:bg-gray-700" } flex cursor-pointer p-3 hover:bg-gray-200 dark:hover:bg-gray-800`}>{subItem.title}</Link>
                       ))}
                     </div>
                   </div>
                 ) : (
-                  <Link href={`/${item.href}`} className={`${ item.href === currentPageset[0]?.pageset && "bg-gray-200 dark:bg-gray-700" } flex cursor-pointer p-3 hover:bg-gray-200`}>{item.title}</Link>
+                  <Link href={`/${item.href}`} className={`${ item.href === currentPageset[0]?.pageset && "bg-gray-200 dark:bg-gray-700" } flex cursor-pointer p-3 hover:bg-gray-200 dark:hover:bg-gray-800`}>{item.title}</Link>
                 )}
               </div>
             ))}
--- components/Menu/MobileMenuItem.tsx
+++ components/Menu/MobileMenuItem.tsx
@@ -27,16 +27,16 @@ export const MobileMenuItem = ({ ddItem, currentPageset, currentPage, onShow }: 
       <div className="flex">
         <Link href={`/${ddItem.href}`} className={`${ ddItem.href === currentPageset && "bg-gray-200 dark:bg-gray-700" } grow px-4 py-3`} onClick={() => onShow()}>{ddItem.title}</Link>
 
-        <div className={`${opened ? "bg-gray-100" : ""} cursor-pointer p-2`} onClick={toggleMenu}>
+        <div className={`${opened ? "bg-gray-100 dark:bg-gray-900" : ""} cursor-pointer p-2`} onClick={toggleMenu}>
           <svg fill="currentColor" width="16" height="16" viewBox="0 0 16 16" className="m-2">
             <path d="M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z"></path>
           </svg>
         </div>
       </div>
 
-      <div className={`${ opened ? "flex" : "hidden" } w-full flex-col bg-white pl-6`}>
+      <div className={`${ opened ? "flex" : "hidden" } w-full flex-col bg-gray-100 dark:bg-gray-900 pl-6`}>
         {ddItem?.subitems?.map((subItem, key) => (
-          <Link key={key} href={subItem.href} className={`${ subItem.href === currentPage && "bg-gray-200 dark:bg-gray-700" } bg-gray-100 px-4 py-3`} onClick={() => onShow()}>{subItem.title}</Link>
+          <Link key={key} href={subItem.href} className={`${ subItem.href === currentPage && "bg-gray-200 dark:bg-gray-700" } bg-gray-100 dark:bg-gray-900 px-4 py-3`} onClick={() => onShow()}>{subItem.title}</Link>
         ))}
       </div>
     </div>
