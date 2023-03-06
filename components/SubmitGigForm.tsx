96c17a20fa340d67d54508c0174158f55e8607bd
vberkoz <vberkoz@gmail.com>
2023-03-06 13:59:10 +0200

    Update select width

--- components/SubmitGigForm.tsx
+++ components/SubmitGigForm.tsx
@@ -73,7 +73,7 @@ export default function SubmitGigForm () {
                   name="service" 
                   id="service"
                   className="
-                    py-3 px-4 appearance-none
+                    py-3 px-4 appearance-none w-full
                     bg-gray-200 dark:bg-gray-800 
                     outline-none focus:ring-2 ring-inset ring-green-600 dark:ring-yellow-400 
                     border-b focus:border-0 border-gray-500
