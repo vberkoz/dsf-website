e1922aeaf97582ae3ccfec492aa2d6bd4d8d5520
vberkoz <vberkoz@gmail.com>
2025-09-13 00:20:08 +0300

    feat(analytics): add google tag manager integration
    Implement GTM tracking to enable analytics and user behavior monitoring

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const GTM_ID = 'G-F5SWS2DE0M';
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GTM_ID}');
+            `,
+          }}
+        />
+      </Head>
       <body className="overflow-x-hidden bg-gray-100 dark:bg-gray-900">
         <Main />
         <NextScript />
